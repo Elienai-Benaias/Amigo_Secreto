171a875019ab3c68abcfb4d3bc5163c532aed478
Jonathan Matamala <jonathanmatamala.2812@gmail.com>
2025-09-06 15:51:12 -0400

    definicion de variables y delineamiento de funciones a codificar

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+//Definicion de variables
+//Variable para ingresar nombres
+let listaDeNombres = []
+//Variable para guardar numero aleatorio
+let subIndiceAmigos = 0
+//Definicion de funciones
+//Funcion ingresar nombre
+
+//Funcion validacion de nombre ingresado
+//Funcion muestra lista de nombres ingresados en pantalla
+//Funcion sortea Amigo Secreto
+//Funcion genera numero aleatorio
